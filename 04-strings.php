9899000753d3aaf1d3da498eec69d7dc4b060add
DiHolanda <diegopessoal@protonmail.com>
2022-03-16 15:44:11 -0300

    Exemplos com Strings - Uma string pode ser formatada com quebras de linha entre as aspas

---
+++ 04-strings.php
@@ -0,0 +1,12 @@
+<?php 
+	
+	echo "qualquer coisa";
+
+	$variavel = "qualquer coisa salva em variavel
+	e ainda em 
+	várias linhas
+	que serão quebrada inclusive no terminal";
+
+	echo $variavel;
+
+ ?>
