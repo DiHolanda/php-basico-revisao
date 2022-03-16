8e692dcee9f6ed8668e9913473e23821fc98555a
DiHolanda <diegopessoal@protonmail.com>
2022-03-16 15:53:19 -0300

    Exemplos de Strings - Concatenações de strings com variáveis, texto direto, ou junção de variáveis com texto

--- 04-strings.php
+++ 04-strings.php
@@ -2,11 +2,24 @@
 	
 	echo "qualquer coisa";
 
-	$variavel = "qualquer coisa salva em variavel
+	$variavel = "
+	qualquer coisa salva em variavel
 	e ainda em 
 	várias linhas
 	que serão quebrada inclusive no terminal";
 
 	echo $variavel;
+
+	$string1 = "PHP";
+	$string2 = "Básico";
+
+	echo "\n";
+	echo $string1 . $string2;
+
+	echo "\n";
+	echo "PHP" . " básico";
+
+	echo "\n";
+	echo $string1 . " " . $string2;
 
  ?>
