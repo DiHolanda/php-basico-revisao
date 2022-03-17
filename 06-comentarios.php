846bbfa39487ac94f61f9b7759536ef20fbdcb23
DiHolanda <diegopessoal@protonmail.com>
2022-03-17 09:57:07 -0300

    Comentarios - apenas demonstrações de como usar comentários no código PHP

---
+++ 06-comentarios.php
@@ -0,0 +1,19 @@
+<?php 
+
+	// comentário de uma linha
+	/* comentário multilinha
+	pode quebrar a linha
+	que tudo que estiver dentro deste bloco
+	será considerado comentário */
+
+	$instrucao = 1 + 1; //comentário ao lado de instrução para explicar o que deve fazer
+
+	// comentários devem ser o mais breve possível e usado apenas quando necessário
+	// assim evita poluir o código com mais comentário do que instrução
+
+	/* !!!!!!!!!!!!!!!!!!!!
+		pode-se inventar um demarcador dentro do comentário para chamar atenção
+		e explicar ou advertir sobre um código complexo
+	!!!!!!!!!!!!!!!!!!!!!!! */
+
+ ?>
