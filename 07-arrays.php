4517d512b3b71d5202c3f876ea3f3f2c3401462c
DiHolanda <diegopessoal@protonmail.com>
2022-03-17 10:09:57 -0300

    Arrays - atribuindo valores em uma variável do tipo array

---
+++ 07-arrays.php
@@ -0,0 +1,17 @@
+<?php 
+	
+	// declara uma variável que será um array
+	$vetor = array();
+
+	// com array vazio, o echo exibirá um alerta e que a variável é do tipo array
+	echo $vetor;
+
+	// atribui valores para o array
+	$vetor = array(20, 40, 50, "Di Holanda", true);
+
+	echo "\n";
+
+	// mas mesmo assim dá o alerta e exibe apenas que variável é do tipo array
+	echo $vetor;
+
+ ?>
