54eeb98c50d69b0604d422a7c13f8c4397c62411
DiHolanda <diegopessoal@protonmail.com>
2022-03-17 10:53:58 -0300

    Arrays - maneiras diferentes de atribuir array - bem como atribuir e exibir um dos valores na mesma linha de comando

--- 07-arrays.php
+++ 07-arrays.php
@@ -9,6 +9,15 @@
 	// atribui valores para o array
 	$vetor = array(20, 40, 50, "Di Holanda", true);
 
+	/* outr forma de declarar os valores em arrays
+	é em várias linhas
+	delimitando cada valor com , */
+	$vetor = array(
+		50,
+		"Di Holanda",
+		true
+	);
+
 	echo "\n";
 
 	// mas mesmo assim dá o alerta e exibe apenas que variável é do tipo array
@@ -18,5 +27,10 @@
 
 	// para exibir um valor dentro do array, precisa informar qual a posição do valor a ser mostrado
 	echo $vetor[0]; // lembrando que a posição começa a partir do zero
+
+	echo "\n";
+
+	// atribui valores num array e exibe um valor na mesma linha de comando
+	echo $vetorInvocado = array(false, false, true)[2];
 
  ?>
