882a172df44d9f4c002153a128860f921ffa0d28
DiHolanda <diegopessoal@protonmail.com>
2022-03-18 13:51:07 -0300

    Passar valores entre páginas php - demonstração da variável $_GET

---
+++ 10-recebe-info.php
@@ -0,0 +1,7 @@
+<?php 
+
+	// a variável GET é um array que coleta valores passados na url da página
+	// exemplo: http://localhost:8000/10-recebe-info.php?nome=Di&sobrenome=Holanda
+	var_dump($_GET);
+
+ ?>
