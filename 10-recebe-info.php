4d69f1264a17c957f352c484007853fc5cf912ef
DiHolanda <diegopessoal@protonmail.com>
2022-03-18 13:53:46 -0300

    Receber valores entre páginas PHP - mostrar valores recebidos pelo $_GET

--- 10-recebe-info.php
+++ 10-recebe-info.php
@@ -2,6 +2,10 @@
 
 	// a variável GET é um array que coleta valores passados na url da página
 	// exemplo: http://localhost:8000/10-recebe-info.php?nome=Di&sobrenome=Holanda
-	var_dump($_GET);
-
+	//var_dump($_GET);
  ?>
+
+ <h1> <?php 
+ 			echo $_GET["nome"] . " " .
+ 			$_GET["sobrenome"];
+ 		 ?> </h1>
