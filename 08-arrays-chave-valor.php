24df775d990912ebc297d56c359b80f952a9a8e5
DiHolanda <diegopessoal@protonmail.com>
2022-03-18 09:54:14 -0300

    Chaves em array - exemplo de atribuição de chaves padrão para o array

---
+++ 08-arrays-chave-valor.php
@@ -0,0 +1,15 @@
+<?php 
+	/* definição de chaves em um array PHP
+		A posição correspondente a cada valor de um array em PHP é chamada de chave
+		Que por padrão é uma sequência numérica a partir do zero
+		Mas dá para definir as chaves em array no PHP
+	*/
+	// as chaves por padrão em arrays seria assim:
+	$arraysChavesPadrao = array(
+		0 => 150,
+		1 => "Di Holanda",
+		2 => true
+	);
+
+	echo $arraysChavesPadrao[1];
+ ?>
