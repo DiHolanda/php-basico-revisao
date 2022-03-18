fcfdd029773b5b9534a4dfeb2b82f94764960f1b
DiHolanda <diegopessoal@protonmail.com>
2022-03-18 11:17:38 -0300

    HTML com PHP - Criado um arquivo PHP que tem html também

---
+++ 09-php-html.php
@@ -0,0 +1,12 @@
+<html lang="pt_BR">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>PHP com HTML</title>
+</head>
+<body>
+	<?php 
+		echo 1 + 1;
+	 ?>
+</body>
+</html>
