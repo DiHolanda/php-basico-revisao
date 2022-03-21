424edc930aacc942a439df0722f50741ec66f764
DiHolanda <diegopessoal@protonmail.com>
2022-03-21 09:16:32 -0300

    Comparadores - uso do comparador !=

--- 12-comparacao.php
+++ 12-comparacao.php
@@ -3,10 +3,22 @@
 	$a = 100;
 	$b = 80;
 
+	// compara se valores são iguais
 	if($a == $b){
 		echo $a . " é igual a " . $b;
 	}
 	else {
 		echo $a . " é diferente de " . $b;
+	}
+
+	echo "\n";
+
+	// compara se valores são diferentes
+	if($a != $b){
+		echo $a . " é diferente de " . $b;
+	}
+	else {
+		
+		echo $a . " é igual a " . $b;
 	}
  ?>
