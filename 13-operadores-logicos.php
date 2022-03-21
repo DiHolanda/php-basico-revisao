0141a84c82e3557c792ae19a392a47c456a4b922
DiHolanda <diegopessoal@protonmail.com>
2022-03-21 10:54:06 -0300

    Operadores lógicos - operador lógico && com duas comparações

--- 13-operadores-logicos.php
+++ 13-operadores-logicos.php
@@ -3,11 +3,29 @@
 	$domingo = true;
 	$diaQuente = true;
 
+	// exemplo simples com booleans
 	if ($domingo && $diaQuente) {
 		echo "Vou dar banho no Het :D";
 	}
 	else {
 		echo "Não vou dar banho no Het :(";
+	}
+
+	echo "\n";
+
+	// duas comparações síncronas
+	$mediaMinima = 7;
+	$frequenciaMinima = 75;
+
+	$mediaAluno = 3;
+	$frequenciaAluno = 50;
+
+	if($mediaAluno >= $mediaMinima && 
+		$frequenciaAluno >= $frequenciaMinima) {
+		echo "Aluno aprovado";
+	}
+	else {
+		echo "Aluno reprovado";
 	}
 
  ?>
