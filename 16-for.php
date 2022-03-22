933e8470e11f245a9d06cccc0e6fa07c7f402063
DiHolanda <diegopessoal@protonmail.com>
2022-03-22 10:39:01 -0300

    Laços de Repetição - fazendo a feira com for

--- 16-for.php
+++ 16-for.php
@@ -3,10 +3,38 @@
 	$i = 0;
 	$tabuada = 1;
 
-	for($i; $i <= 10; $i++) {
+	/*for($i; $tabuada <= 10; $i++) {
+		
 		echo $tabuada . " x " .
 			$i . " = " .
 			($tabuada * $i) . "<br />";
+
+		// ao calcular o 10 da tabuada atual, faz a tabuada do número seguinte
+		if($i == 10) {
+			echo "<br /> <hr /> <br />";
+			$i = 0;
+			$tabuada++;
+		}
+	}*/
+
+	$feira = [
+		"Manga",
+		"Caqui",
+		"Mamão",
+		"Abacate",
+		"Pera",
+		"Banana"
+	];
+
+	echo "Eu quero: ";
+
+	// laço para passar por todas as posições de um array
+	for($i = 0; $i < count($feira); $i++){
+		if($i == count($feira)-1){
+			echo $feira[$i];
+			continue;
+		}
+		echo $feira[$i] . ", ";
 	}
 
  ?>
