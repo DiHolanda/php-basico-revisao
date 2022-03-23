b0b0a1a5f20314591e85986ffad9121aa9d9aa52
DiHolanda <diegopessoal@protonmail.com>
2022-03-23 16:46:08 -0300

    Mais sobre post - recebeu os dados de POST em variáveis e os exibiu na página

--- 20-recebe-post.php
+++ 20-recebe-post.php
@@ -1,5 +1,23 @@
 <?php 
 
-	var_dump($_POST);
+	$nome = $_POST["nome"];
+	$email = $_POST["email"];
+	$interesses = $_POST["interesse"];
+	$onde_conheceu = $_POST["onde_conheceu"];
+	$mensagem = $_POST["mensagem"];
+	$redirecionar = $_POST["redirecionar"];
+
+	echo "<strong> Nome: </strong> " . $nome . "<br>";
+	echo "<strong> Email: </strong> " . $email . "<br>";
+	echo "<strong> Interesses: </strong> <ul> ";
+	foreach ($interesses as $interesse){
+		echo " <li> " . $interesse . " </li> ";
+	}
+	echo "</ul>";
+	
+	echo "<strong> Onde Conheceu: </strong> " . $onde_conheceu . "<br>";
+	echo "<strong> Mensagem: </strong> " . $mensagem . "<br>";
+	echo "<strong> Redirecionar para: </strong> " . $redirecionar . "<br>";
+	
 
  ?>
