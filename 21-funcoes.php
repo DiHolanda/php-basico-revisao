4ce3fcab6fd06a98852cb509b98b48766f625e13
DiHolanda <diegopessoal@protonmail.com>
2022-03-24 14:30:52 -0300

    Funções em PHP - chamar a função quantas vezes necessárias, sob parâmetros diferentes

--- 21-funcoes.php
+++ 21-funcoes.php
@@ -1,9 +1,12 @@
 <?php 
 
 	function escreverNoBN($nome, $causa) {
-		echo "Black Note: $nome morrerá por $causa";
+		echo "Black Note: $nome morre por $causa <hr>";
 	}
 
 	escreverNoBN("Hideyoshi Toyotomi", "causas naturais");
+	escreverNoBN("Nobunaga Oda", "traição");
+	escreverNoBN("Mitsunari Ishida", "hara-kiri");
+	escreverNoBN("Ieyasu Tokugawa", "lesões pós-batalha");
 
  ?>
