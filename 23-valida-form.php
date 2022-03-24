c8c889a569be3455ae5bafc47dab79b8e06b3d8e
DiHolanda <diegopessoal@protonmail.com>
2022-03-24 15:35:13 -0300

    Validação - criado função para eliminar tags e evitar code_injection

--- 23-valida-form.php
+++ 23-valida-form.php
@@ -1,15 +1,24 @@
 <?php 
 
-	$nome = $_POST["nome"];
-	$email = $_POST["email"];
+	$nome = limpatag($_POST["nome"]);
+	$email = limpatag($_POST["email"]);
 	$interesses = $_POST["interesse"] ?? null;
 	$onde_conheceu = $_POST["onde_conheceu"];
-	$mensagem = $_POST["mensagem"];
+	$mensagem = limpatag($_POST["mensagem"]);
 	$redirecionar = $_POST["redirecionar"] ?? null;
+
+	function limpaTag($string){
+		return strip_tags($string);
+	}
 
 	if(empty(trim($nome))){
 		echo "Favor informar o nome";
 	}
+
+	// demonstração para validar apenas se o parâmetro é null
+	/*if(is_null($interesses)){
+		echo "Larga de ser desinteressado e escolha o seu interesse >:|";
+	}*/
 
 	
  ?>
