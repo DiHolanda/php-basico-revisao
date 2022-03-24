950daed8826c6f5203db1d846221e92940193b41
DiHolanda <diegopessoal@protonmail.com>
2022-03-24 15:44:36 -0300

    Validação - função criada para validar todo o formulário

--- 23-valida-form.php
+++ 23-valida-form.php
@@ -2,18 +2,37 @@
 
 	$nome = limpatag($_POST["nome"]);
 	$email = limpatag($_POST["email"]);
-	$interesses = $_POST["interesse"] ?? null;
+	$interesses = $_POST["interesse"] ?? $_POST["interesse"] = [""];
 	$onde_conheceu = $_POST["onde_conheceu"];
 	$mensagem = limpatag($_POST["mensagem"]);
-	$redirecionar = $_POST["redirecionar"] ?? null;
+	$redirecionar = $_POST["redirecionar"] ?? "";
 
 	function limpaTag($string){
 		return strip_tags($string);
 	}
 
-	if(empty(trim($nome))){
-		echo "Favor informar o nome";
+
+	function validaCampo($campo){
+		if(empty(trim($campo))){
+			return "Favor preencher o campo";
+		}
+		else {
+			return $campo;
+		}
+
 	}
+
+	echo "<strong> Nome: </strong> " . validaCampo($nome) . "<br>";
+	echo "<strong> Email: </strong> " . validaCampo($email) . "<br>";
+	echo "<strong> Interesses: </strong> <ul> ";
+	foreach ($interesses as $interesse){
+		echo " <li> " . validaCampo($interesse) . " </li> ";
+	}
+	echo "</ul>";
+	
+	echo "<strong> Onde Conheceu: </strong> " . validaCampo($onde_conheceu) . "<br>";
+	echo "<strong> Mensagem: </strong> " . validaCampo($mensagem) . "<br>";
+	echo "<strong> Redirecionar para: </strong> " . validaCampo($redirecionar) . "<br>";
 
 	// demonstração para validar apenas se o parâmetro é null
 	/*if(is_null($interesses)){
