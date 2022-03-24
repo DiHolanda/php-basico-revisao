7011e0b79ee91c97bdf748f739538c280665c38a
DiHolanda <diegopessoal@protonmail.com>
2022-03-24 15:25:57 -0300

    Validação - definindo arrays vazios quando não preenchidos no formulário para posterior validação

--- 23-valida-form.php
+++ 23-valida-form.php
@@ -2,10 +2,10 @@
 
 	$nome = $_POST["nome"];
 	$email = $_POST["email"];
-	$interesses = $_POST["interesse"];
+	$interesses = $_POST["interesse"] ?? null;
 	$onde_conheceu = $_POST["onde_conheceu"];
 	$mensagem = $_POST["mensagem"];
-	$redirecionar = $_POST["redirecionar"];
+	$redirecionar = $_POST["redirecionar"] ?? null;
 
 	if(empty(trim($nome))){
 		echo "Favor informar o nome";
